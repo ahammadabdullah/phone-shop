519a25909b062d7347fdd0f87141a29305dc8331
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2023-09-24 05:06:10 +0600

    show more button added

--- src/Pages/Favorites/Favorites.jsx
+++ src/Pages/Favorites/Favorites.jsx
@@ -5,6 +5,7 @@ import Phone from "../../Components/Phones/Phone";
 const Favorites = () => {
   const [favorites, setFavorites] = useState([]);
   const [noFound, setNoFound] = useState(false);
+  const [isShow, setIsShow] = useState(false);
   useEffect(() => {
     const favoritesItems = JSON.parse(localStorage.getItem("favorites"));
     if (favoritesItems) {
@@ -12,11 +13,13 @@ const Favorites = () => {
     } else {
       setNoFound("No Data Found");
     }
-  }, [favorites]);
+  }, []);
   const handleClearFavorite = () => {
     localStorage.clear();
     setFavorites([]);
+    setNoFound("No Data Found");
   };
+
   return (
     <div className="mt-6">
       {noFound ? (
@@ -33,11 +36,23 @@ const Favorites = () => {
               Delete All
             </button>
           )}
-          <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-            {favorites.map((phone) => (
-              <PhoneCard key={phone.id} phone={phone}></PhoneCard>
-            ))}
+          <div className="my-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {isShow
+              ? favorites.map((phone) => (
+                  <PhoneCard key={phone.id} phone={phone}></PhoneCard>
+                ))
+              : favorites
+                  .slice(0, 2)
+                  .map((phone) => (
+                    <PhoneCard key={phone.id} phone={phone}></PhoneCard>
+                  ))}
           </div>
+          <button
+            onClick={() => setIsShow(!isShow)}
+            className="px-5 py-2 rounded-lg bg-green-200 block mx-auto"
+          >
+            {isShow ? "Show Less" : "Show More"}
+          </button>
         </div>
       )}
     </div>
